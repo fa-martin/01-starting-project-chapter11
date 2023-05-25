fac1261730530fa5be1a500bdcb53cefaed79b0b
fa-martin <f.a.martin@hotmail.com>
2023-05-25 17:33:54 -0300

    Adding some header to my app

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import CartIcon from "../ItemContainer/CartIcon";
+import classes from '../Layout/HeaderCartButton.module.css';
+
+const HeaderCartButton = (props) => {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>3.2</span>
+    </button>
+  );
+};
+export default HeaderCartButton;
